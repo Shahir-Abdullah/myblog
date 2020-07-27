7e2180df80b90278fbc0b2c7a333dbf6f256ad0c
shahir-abdullah <shahir2561@gmail.com>
2020-07-27 11:03:37 +0600

    Updates

---
+++ precache-manifest.4921633213cf73ae3573afcfc928f867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a907a5b114f42016b2e57060eba00b13",
+    "url": "/myblog/index.html"
+  },
+  {
+    "revision": "c18872e7a61bd159d2e7",
+    "url": "/myblog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8e2997e377c96826a20e",
+    "url": "/myblog/static/js/2.027d604f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/myblog/static/js/2.027d604f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18872e7a61bd159d2e7",
+    "url": "/myblog/static/js/main.efbc6758.chunk.js"
+  },
+  {
+    "revision": "f6228e94ef8e0d9afd5d",
+    "url": "/myblog/static/js/runtime-main.b6980ad7.js"
+  }
+]);
